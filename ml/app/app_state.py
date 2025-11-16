40974fe09e8d45cfe28842bc19e23190f337eb8d
KawaichMag <kawaichmag@gmail.com>
2025-11-17 00:09:28 +0300

    improved uv

--- ml/app/app_state.py
+++ ml/app/app_state.py
@@ -1,7 +1,8 @@
-from transformers import AutoModelForCausalLM
 import torch
+from transformers import AutoModelForCausalLM
 
 torch.cuda.is_available()
+
 
 def init_models(app):
     model_id = "vikhyatk/moondream2"
@@ -12,5 +13,5 @@ def init_models(app):
         revision=revision,
         trust_remote_code=True,
         dtype=torch.float16,
-        device_map={"": "cuda"} if torch.cuda.is_available() else {"": "cpu"}
+        device_map={"": "cuda"} if torch.cuda.is_available() else {"": "cpu"},
     )
